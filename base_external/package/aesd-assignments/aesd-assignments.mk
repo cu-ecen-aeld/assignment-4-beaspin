7c3e32816562702117ee6066949748a97cf0b549
vboxuser <beaspin@icloud.com>
2025-03-06 12:08:42 +0100

    part 8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,6 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
+BR_NO_CHECK_HASH_FOR = aesd-assignments
 AESD_ASSIGNMENTS_VERSION = 'master'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
@@ -13,6 +14,10 @@ AESD_ASSIGNMENTS_VERSION = 'master'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignment-4-beaspin.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+define AESD_ASSIGNMENTS_EXTRACT_CMDS
+	git clone --branch $(AESD_ASSIGNMENTS_VERSION) $(AESD_ASSIGNMENTS_SITE) $(@D)
+endef
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
@@ -22,10 +27,10 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-        $(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/usr/bin/
-        $(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/usr/bin/
-        $(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/usr/bin/
-        $(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/tester.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 endef
 
